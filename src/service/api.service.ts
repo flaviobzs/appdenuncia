197e7b150982df6f985f19aa4998a8a42de352d2
Flavio B Santos <bzsflavio@gmail.com>
2021-06-08 19:24:55 -0300

    identify

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseURL = `${process.env.NEXT_PUBLIC_HOST_API}`
+// const baseURL = `${process.env.NEXT_PUBLIC_HOST_API}`
+const baseURL = 'http://134.209.69.80:8000'
 
 const apiClient = axios.create({
   baseURL,
